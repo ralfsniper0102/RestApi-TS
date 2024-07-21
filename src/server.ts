339a6db99cc56fa106fcd7322ca7114bd3b19cac
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2024-07-21 01:55:12 -0300

    Fix: put interpolation

--- src/server.ts
+++ src/server.ts
@@ -3,4 +3,4 @@ import "dotenv";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(3001, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
