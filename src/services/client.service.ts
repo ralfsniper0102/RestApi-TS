15a9892c49700cd2ea274a5a45409b3025bef45f
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2024-07-22 19:31:46 -0300

    Feat: Verification Client Exist / GetById - Client

--- src/services/client.service.ts
+++ src/services/client.service.ts
@@ -24,6 +24,11 @@ class ClientService {
     const { error } = schema.client.validate(client);
     if(error) return resp(400, error.message);
 
+    if(client.email){
+        const clientExists = await this.model.findOne({where: {email: client.email}});
+        if(clientExists) return resp(400, "Client already exists");
+    }
+
     let clientCreated;
     try{
         clientCreated = await this.model.create({...client});
@@ -33,6 +38,20 @@ class ClientService {
     }
 
     return resp(201, undefined);
+  }
+
+  async getById(id: number) {
+    let client;
+    try{
+        client = await this.model.findByPk(id);
+
+        if(!client) return resp(404, "Client not found");
+    }
+    catch(error){
+        return resp(500, "Error searching client");
+    }
+
+    return resp(200, client);
   }
 }
 
