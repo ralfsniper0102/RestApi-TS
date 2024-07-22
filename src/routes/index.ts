eadff2245fcadd1cd727ab0781cf024513b65fb6
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2024-07-22 02:35:35 -0300

    Feat: Update index.ts Routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import userRouter from "./userRouter";
+import clientRouter from "./clientRouter";
 
 const router = Router();
 
 router.use(userRouter);
+router.use(clientRouter);
 
 export default router;
