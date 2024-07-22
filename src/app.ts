e838007d7347ac2962cf79f187a9c8fc42bc3f75
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2024-07-22 19:18:49 -0300

    Feat: Change Swagger Theme, Put jwt input and Remove Explorer

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
-import swaggerUi from 'swagger-ui-express';
-import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from "swagger-ui-express";
+import swaggerJsdoc from "swagger-jsdoc";
 import router from "./routes";
-const { SwaggerTheme, SwaggerThemeNameEnum } = require('swagger-themes');
+const { SwaggerTheme, SwaggerThemeNameEnum } = require("swagger-themes");
 
 const app = express();
 
@@ -16,24 +16,38 @@ const options = {
       version: '1.0.0',
       description: 'API documentation for your TS-EXPRESS-SEQUELIZE app',
     },
+
+    components: {
+      securitySchemes: {
+        JWT: {
+          type: 'apiKey',
+          in: 'header',
+          name: 'Authorization',
+          description: 'JWT Authorization header using the Bearer scheme. Example: "Authorization: {token}"'
+        },
+      },
+    },
+    security: [{
+      JWT: []
+    }],
   },
   apis: ['./src/routes/*.ts'],
 };
 
 const swaggerTheme = {
-  explorer: true,
-  customCss: theme.getBuffer(SwaggerThemeNameEnum.DARK)
-}
+  customCss: theme.getBuffer(SwaggerThemeNameEnum.DRACULA),
+  
+};
 
 const specs = swaggerJsdoc(options);
 
 app.use(express.json());
 app.use(cors());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs, swaggerTheme));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs, swaggerTheme));
 app.use(router);
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    res.status(500).json({ error: err.message });
-    next();
+  res.status(500).json({ error: err.message });
+  next();
 });
 
 export default app;
