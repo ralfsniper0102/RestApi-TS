36a175d8e8e462678e4b4b319f18b21365520102
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2024-07-22 19:32:48 -0300

    Feat: Create respWithNameField

--- src/utils/resp.ts
+++ src/utils/resp.ts
@@ -3,7 +3,11 @@ const respM = (s: number, m: unknown) => ({
   status: s,
   message: { message: m },
 });
-
 const respNoMessage = (s: number) => ({ status: s });
+const respWithNameField = (s: number, m: unknown, n: string) => ({
+  status: s,
+  message: { [n]: m },
+});
 
-export { resp, respM, respNoMessage };
+
+export { resp, respM, respNoMessage, respWithNameField };
