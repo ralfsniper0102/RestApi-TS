add797d5eb3051be9a4e38eef60bea99c634dc8d
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2024-07-22 02:32:34 -0300

    Feat: Create ClientController with your services

---
+++ src/controller/client.controller.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from "express";
+import ClientService from "../services/client.service";
+
+class ClientController {
+  private service = new ClientService();
+
+  async getAll(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { status, message } = await this.service.getAll();
+      res.status(status).json(message);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { status, message } = await this.service.create(req.body);
+      res.status(status).json(message);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+export default ClientController;
