0ed7466239fb21eb75bcd0ae7fb003c933c87e00
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2024-07-22 19:20:54 -0300

    Feat: getById from User Controller

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -5,15 +5,6 @@ import md5 from "md5";
 class UserController {
   private service = new UserService();
 
-  /**
-   * @swagger
-   * /user:
-   *   get:
-   *     summary: Retrieve a list of users
-   *     responses:
-   *       200:
-   *         description: A list of users
-   */
   async getAll(req: Request, res: Response, next: NextFunction) {
     try {
       const { status, message } = await this.service.getAll();
@@ -35,6 +26,14 @@ class UserController {
   async create(req: Request, res: Response, next: NextFunction) {
     try {
       const { status, message } = await this.service.create(req.body);
+      res.status(status).json(message);
+    } catch (error) {
+      next(error);
+    }
+  }
+  async getById(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { status, message } = await this.service.getById(req.params.id);
       res.status(status).json(message);
     } catch (error) {
       next(error);
