da9f7bf767ae2dbf18e3fe4f0256666670137e27
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2024-07-22 19:29:56 -0300

    Feat: Create new route - GetUserById / Fix: JSDOC

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import UserController from "../controller/user.controller";
+import { verifyToken } from "../jwt/jwt";
 
 const control = new UserController();
 
@@ -7,7 +8,7 @@ const userRouter = Router();
 
 /**
  * @swagger
- * api/User/GetAllUser:
+ * /api/User/GetAllUsers:
  *   get:
  *     summary: Retrieve all users
  *     tags: [Users]
@@ -21,11 +22,11 @@ const userRouter = Router();
  *               items:
  *                 $ref: '#/components/schemas/User'
  */
-userRouter.get("api/User/GetAllUsers", control.getAll.bind(control));
+userRouter.get("/api/User/GetAllUsers", verifyToken, control.getAll.bind(control));
 
 /**
  * @swagger
- * api/User/Login:
+ * /api/User/Login:
  *   post:
  *     summary: User login
  *     tags: [Users]
@@ -41,11 +42,11 @@ userRouter.get("api/User/GetAllUsers", control.getAll.bind(control));
  *       401:
  *         description: Unauthorized
  */
-userRouter.post("api/User/Login", control.login.bind(control));
+userRouter.post("/api/User/Login", control.login.bind(control));
 
 /**
  * @swagger
- * api/User/CreateUser:
+ * /api/User/CreateUser:
  *   post:
  *     summary: Create a new user
  *     tags: [Users]
@@ -61,8 +62,32 @@ userRouter.post("api/User/Login", control.login.bind(control));
  *       400:
  *         description: Bad request
  */
-userRouter.post("api/User/CreateUser", control.create.bind(control));
+userRouter.post("/api/User/CreateUser", control.create.bind(control));
 
+/**
+ * @swagger
+ * /api/User/GetUserById/{id}:
+ *   get:
+ *     summary: Get a user by ID
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: Id of the client to retrieve
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved client
+ *       400:
+ *         description: Bad request
+ *       404:
+ *         description: Client not found
+ *       500:
+ *         description: Internal server error
+ */
+userRouter.get("/api/User/GetUserById/:id", verifyToken, control.getById.bind(control));
 
 /**
  * @swagger
