7e479bfa1222337ee581d940b59fe5b3600cf9be
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2024-07-22 02:35:03 -0300

    Feat: Client Router and your JSDOC

---
+++ src/routes/clientRouter.ts
@@ -0,0 +1,120 @@
+import { Router } from "express";
+import ClientController from "../controller/client.controller";
+
+const control = new ClientController();
+
+const clientRouter = Router();
+
+/**
+ * @swagger
+ *   /client:
+ *      get:
+ *          summary: Retrieve all clients
+ *          tags: [Clients]
+ *          responses:
+ *              200:
+ *                  description: A list of clients
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: array
+ *                              items:
+ *                                  $ref: '#/components/schemas/GetClients'
+ */
+clientRouter.get("/client", control.getAll.bind(control));
+
+
+/**
+ * @swagger
+ * /createClient:
+ *   post:
+ *     summary: Create a new client
+ *     tags: [Clients]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/CreateClient'
+ *     responses:
+ *       201:
+ *         description: Client created successfully
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+ */
+clientRouter.post("/createClient", control.create.bind(control));
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     GetClients:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: integer
+ *         name:        
+ *           type: string
+ *         email:
+ *           type: string
+ *         phone:
+ *           type: string
+ *         address:
+ *           type: string
+ *         city:
+ *           type: string
+ *         state:
+ *           type: string
+ *         zip:
+ *           type: string
+ *         country:
+ *           type: string
+ *         createdAt:
+ *           type: string
+ *           example: "2023-06-07T12:34:56Z"
+ *         updatedAt:
+ *           type: string
+ *           example: "2023-06-07T12:34:56Z"
+ *         createdById:
+ *           type: integer
+ *     CreateClient:
+ *       type: object
+ *       properties:
+ *         name:
+ *           type: string
+ *         email:
+ *           type: string
+ *         mobilePhone:
+ *           type: string
+ *         phone:
+ *           type: string
+ *         address:
+ *           type: string
+ *         city:
+ *           type: string
+ *         state:
+ *           type: string
+ *         zip:
+ *           type: string
+ *         country:
+ *           type: string
+ *         createdAt:
+ *           type: string
+ *           example: "2023-06-07T12:34:56Z"
+ *         createdById:
+ *           type: integer
+ *       required:
+ *         - name
+ *         - email
+ *         - mobilePhone
+ *         - address
+ *         - city
+ *         - state
+ *         - zip
+ *         - country
+ *         - createdById  
+ */
+
+export default clientRouter;
