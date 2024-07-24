ed4d4ccf5b2c406104ca1611b8ea4961d0a83f99
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2024-07-24 02:16:44 -0300

    Feat: change Url Swagger

--- src/app.ts
+++ src/app.ts
@@ -51,7 +51,7 @@ const specs = swaggerJsdoc(options);
 
 app.use(express.json());
 app.use(cors());
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs, swaggerTheme));
+app.use("/swagger/index.html", swaggerUi.serve, swaggerUi.setup(specs, swaggerTheme));
 app.use(router);
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   res.status(500).json({ error: err.message });
